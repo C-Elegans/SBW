3226f66972be41a5d0dbcd01262a7e38413dae2d
C-Elegans <mtnolan2640@gmail.com>
2015-09-23 17:46:10 -0400

    change levels

--- ios gl tutorial/Levels/combineJson.py
+++ ios gl tutorial/Levels/combineJson.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import glob,sys
 import re
 
@@ -6,10 +7,10 @@ def natural_sort(l):
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
     
-levels = glob.glob("level*")
+levels = glob.glob("/Users/mnolan/programming/objective-c/ios gl tutorial/ios gl tutorial/Levels/level*")
 levels = natural_sort(levels)
 
-out = open(sys.argv[1], "w")
+out = open("/Users/mnolan/programming/objective-c/ios gl tutorial/ios gl tutorial/levelData.json", "w")
 out.write("[\n")
 for filename in levels:
     infile = open(filename,"r")
